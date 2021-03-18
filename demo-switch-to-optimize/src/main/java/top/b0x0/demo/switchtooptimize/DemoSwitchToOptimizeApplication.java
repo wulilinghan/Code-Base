e2600fdef7c4b76f2c16a27ecbadc12ec4cad8c6
tlh <im.linghan@qq.com>
2021-03-18 22:57:41 +0800

    first commit

---
+++ demo-switch-to-optimize/src/main/java/top/b0x0/demo/switchtooptimize/DemoSwitchToOptimizeApplication.java
@@ -0,0 +1,13 @@
+package top.b0x0.demo.switchtooptimize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSwitchToOptimizeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSwitchToOptimizeApplication.class, args);
+    }
+
+}
---
+++ demo-switch-to-optimize/src/test/java/top/b0x0/demo/switchtooptimize/DemoSwitchToOptimizeApplicationTests.java
@@ -0,0 +1,13 @@
+package top.b0x0.demo.switchtooptimize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSwitchToOptimizeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
