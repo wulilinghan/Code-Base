3a427ef1bde176d6731ac2c7b5f2ff8cfd1c9b8c
tlh <im.linghan@qq.com>
2021-03-21 17:55:15 +0800

    feat: 加解密demo

---
+++ encryption-demo/src/main/java/top/b0x0/demo/encryption/EncryptionDemoApplication.java
@@ -0,0 +1,16 @@
+package top.b0x0.demo.encryption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author musui
+ */
+@SpringBootApplication
+public class EncryptionDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EncryptionDemoApplication.class, args);
+    }
+
+}
