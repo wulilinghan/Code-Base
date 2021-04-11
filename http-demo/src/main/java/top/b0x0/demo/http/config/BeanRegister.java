0f014e2cc74ac07e162f7e8eaca0f24e2ac1bb4e
tlh <im.linghan@qq.com>
2021-04-11 15:37:42 +0800

    feat: add RestTemplate eg

--- http-demo/src/main/java/top/b0x0/demo/http/config/BeanRegister.java
+++ http-demo/src/main/java/top/b0x0/demo/http/config/BeanRegister.java
@@ -1,10 +1,14 @@
 package top.b0x0.demo.http.config;
 
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.util.unit.DataSize;
 import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.MultipartConfigElement;
 
 /**
  * 注册bean
@@ -13,7 +17,7 @@ import org.springframework.web.client.RestTemplate;
  * @since 2021-04-08
  */
 @Component
-public class RestTemplateRegister {
+public class BeanRegister {
 
     @Bean
     public RestTemplate restTemplate(ClientHttpRequestFactory factory) {
@@ -28,5 +32,21 @@ public class RestTemplateRegister {
         // 设置代理
         //factory.setProxy(null);
         return factory;
+    }
+
+    /**
+     * org.apache.tomcat.util.http.fileupload.impl.FileSizeLimitExceededException: The field file exceeds its maximum permitted size of 1048576 bytes., code=400
+     * 设置tomcat上传文件大小限制
+     *
+     * @return /
+     */
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //允许上传的文件最大值 (//KB,MB)
+        factory.setMaxFileSize(DataSize.parse("50MB"));
+        /// 设置总上传数据总大小
+        factory.setMaxRequestSize(DataSize.parse("100MB"));
+        return factory.createMultipartConfig();
     }
 }
